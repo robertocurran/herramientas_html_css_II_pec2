38011a4ca40a473b9688eba56e7f580fcb4fb3d6
root <root@RobPC.localdomain>
2022-12-07 16:44:59 +0000

    Ficheros PEC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,15 +2,10 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
+import * as bootstrap from "bootstrap";
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
-
-+( function() {
+bootstrap.Alert("Loaded bootstrap");
+(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
